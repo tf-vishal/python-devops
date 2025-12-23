e0a82d61876e1c7ffdf114082559fec0f8d61098
tf-vishal <vvishalsharma0609@gmail.com>
2025-12-23 16:21:14 +0530

    API Assignment done API ASsignmnet done

--- day-02/practice/api.py
+++ day-02/practice/api.py
@@ -3,8 +3,15 @@ import requests
 api_url = "https://jsonplaceholder.typicode.com/todos/1" # server URL (API)
 
 response = requests.get(url=api_url)
+req_keys = ["userId", "id", "title"]
 
-for key,value in response.json().items():
-    if key == "userId":
-        if value in [100,200,300]:
-            print("User found")
+data = response.json()
+print(response.json())
+
+for key in req_keys:
+    print(f"{key}: {data.get(key)}")
+
+# for key,value in response.json().items():
+#     if key == "userId":
+#         if value in [100,200,300]:
+#             print("User found")
